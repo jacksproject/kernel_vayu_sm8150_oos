f18737eb0e4775ecfb875d503442b48130231fba
sidex15 <24408329+sidex15@users.noreply.github.com>
2025-12-14 10:14:04 +0800

    [BACKPORT] fs: implement susfs v2.0.0
    * sus_mount and others are complied with the upstream susfs kernel branch v5.10
    * susfs try_umount is restored
    * auto features are deprecated so you have to hide your mounts manually
    
    This susfs backport commit requires a set of backports to comply with the upstream susfs:
    d406904bb175cb9791b5aed4404df3268859d91b fs: Convert namespace IDAs to new API
    9ba899398576d329288b023566d3a731470ad13a fs: Convert unnamed_dev_ida to new API
    e784224542fab4b3888d9103a1f678015221a7d1 ida: Add new API
    ed8fd10d97a50ec424ae38a40351b5de060f9838 ida: Lock the IDA in ida_destroy
    4ba6dccf73a620e565803e5995d5748ecfbb56a2 lib/idr.c: remove simple_ida_loc
    0a8cbf3 proc: less memory for /proc/*/map_files readdir
    37ae244 mm: smaps: split PSS into components
    49a5115 fs/task_mmu: add pkeys header
    6f94042 mm/pkeys: Add an empty arch_pkeys_enabled()
    bbd5aec mm/pkeys, powerpc, x86: Provide an empty vma_pkey() in linux/pkeys.h
    849ca8c mm: /proc/pid/smaps_rollup: convert to single value seq_file
    6071a48 mm: /proc/pid/smaps: factor out common stats printing
    03fd2fb mm: /proc/pid/smaps: factor out mem stats gathering
    95f8be4 mm: /proc/pid/*maps remove is_pid and related wrappers
    27956d2 procfs: add seq_put_hex_ll to speed up /proc/pid/maps
    7f2847d proc: add seq_put_decimal_ull_width to speed up /proc/pid/smaps
    
    For KernelSU, Use my RKSU fork which it includes susfs try_umount:
    https://github.com/sidex15/KernelSU/tree/susfs-rksu-master
    
    Or if you want to patch it within RKSU, use this commit:
    https://github.com/sidex15/KernelSU/commit/8be13a387a7b32f6c70b9077a97c30840254f630

--- kernel/sys.c
+++ kernel/sys.c
@@ -1205,6 +1205,9 @@ static int override_version(struct new_utsname __user *name)
 }
 
 static uint64_t netbpfload_pid = 0;
+#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
+extern void susfs_spoof_uname(struct new_utsname* tmp);
+#endif
 SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 {
 	struct new_utsname tmp;
@@ -1220,6 +1223,9 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 				 current->comm, current->pid, tmp.release);
 		}
 	}
+#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
+	susfs_spoof_uname(&tmp);
+#endif
 	up_read(&uts_sem);
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
 		return -EFAULT;
